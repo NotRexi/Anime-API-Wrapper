600ace85f1d08d0e25daa8732bcf0081875394ce
Rexi <132494143+NotRexi@users.noreply.github.com>
2025-09-20 16:47:42 -0400

    Update models.py

--- animepahe/models.py
+++ animepahe/models.py
@@ -9,21 +9,24 @@ class Anime:
 
 
 class Episode:
-    def __init__(self, id_, anime_id, number, snapshot, audio, duration, url):
+    def __init__(self, id_, anime_id, number, snapshot, duration, url):
         self.id = id_
         self.anime_id = anime_id
         self.number = number
         self.snapshot = snapshot
-        self.audio = audio
         self.duration = duration
         self.url = url
 
 
 class Download:
-    def __init__(self, url, res, fileSize, type, is_dubbed):
+    def __init__(self, url, res, fileSize, is_dubbed):
         self.url = url
         self.res = res
         self.fileSize = fileSize
-        self.type = "Dub" if type else "Sub"
         self.downloadLink = None
         self.is_dubbed = is_dubbed
+
+    @property
+    def label(self):
+        """Dynamic display label based on is_dubbed."""
+        return "Dub" if self.is_dubbed else "Sub"
